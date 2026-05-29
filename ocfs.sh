5154d108597a1d933aef05b166b20e1be4c01a51
username29800 <piapia78490@gmail.com>
2026-05-29 13:17:05 +0900

    switch to name prefix instead of path prefix

--- ocfs.sh
+++ ocfs.sh
@@ -3,7 +3,7 @@ echo "[ ssh.well File-sharing ]"
 echo "connecting to $cdw_session_user@$cdw_session_host:$cdw_session_port via ssh"
 echo "retrieving $dwfs_src"
 ldwell_gen_idopts
-$cdw_xcp -i $cdw_session_id -OCrp -P "$cdw_session_port" $cdw_session_jmp $cdw_session_args "${cdw_session_user}@${cdw_session_host}:$rdwfs_prefix/$dwfs_src" "$dwfs_prefix/$dwfs_dst"'
+$cdw_xcp -i $cdw_session_id -OCrp -P "$cdw_session_port" $cdw_session_jmp $cdw_session_args "${cdw_session_user}@${cdw_session_host}:$rdwfs_prefix$dwfs_src" "$dwfs_prefix$dwfs_dst"'
 alias dwell_get="$dwell_get"
 
 dwell_send='
@@ -11,7 +11,7 @@ echo "[ ssh.well file transfer ]"
 echo "connecting to $cdw_session_user@$cdw_session_host:$cdw_session_port via ssh"
 echo "sending $dwfs_src"
 ldwell_gen_idopts
-$cdw_xcp -i $cdw_session_id -OCrp -P "$cdw_session_port" $cdw_session_jmp $cdw_session_args "$dwfs_prefix/$dwfs_src" ${cdw_session_user}@${cdw_session_host}:"$rdwfs_prefix/$dwfs_dst"
+$cdw_xcp -i $cdw_session_id -OCrp -P "$cdw_session_port" $cdw_session_jmp $cdw_session_args "$dwfs_prefix$dwfs_src" ${cdw_session_user}@${cdw_session_host}:"$rdwfs_prefix$dwfs_dst"
 echo "saved to $dwfs_dst"'
 alias dwell_send="$dwell_send"
 

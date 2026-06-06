bf8d7f9aae5501cd5479a74713a872bd59c4f51c
username29800 <134399459+username29800@users.noreply.github.com>
2026-06-06 18:44:39 +0900

    dev: enable out-of-local initialization of alias scripts

--- src.sh
+++ src.sh
@@ -1,9 +1,14 @@
-. ./lib.sh
-. ./ocsn.sh
-. ./occm.sh
-. ./ocfs.sh
 ldwell_path='.'
-ldwell_config_lfile='./config_list'
-ldwell_config_alias='test'
-ldwell_set_config
-ldwell_load_config
+. "$ldwell_path"/lib.sh
+. "$ldwell_path"/ocsn.sh
+. "$ldwell_path"/occm.sh
+. "$ldwell_path"/ocfs.sh
+
+config_list='config_list'
+
+default_config='test'
+
+ldwell_config_lfile="$ldwell_path/$config_list"
+ldwell_config_alias="$default_config"
+ldwell_call_config
+#ldwell_load_config
